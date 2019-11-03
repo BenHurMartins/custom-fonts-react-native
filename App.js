333d51bb1901b01d4f407b6390ce14069b912909
Ben Hur Martins <benhurmartins@Mac-mini-de-Ben.local>
2019-11-03 16:18:25 -0300

    finished

--- App.js
+++ App.js
@@ -26,88 +26,33 @@ import {
 
 const App: () => React$Node = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <SafeAreaView style={styles.container}>
+      <Text style={{fontFamily: 'BowlbyOneSC-Regular', fontSize: 30}}>
+        BowlbyOneSC
+      </Text>
+      <Text style={{fontFamily: 'Heebo-Black', fontSize: 30}}>Heebo-Black</Text>
+      <Text style={{fontFamily: 'Heebo-Bold', fontSize: 30}}>Heebo-Bold</Text>
+      <Text style={{fontFamily: 'Heebo-ExtraBold', fontSize: 30}}>
+        Heebo-ExtraBold
+      </Text>
+      <Text style={{fontFamily: 'Heebo-Light', fontSize: 30}}>Heebo-Light</Text>
+      <Text style={{fontFamily: 'Heebo-Medium', fontSize: 30}}>
+        Heebo-Medium
+      </Text>
+      <Text style={{fontFamily: 'Heebo-Regular', fontSize: 30}}>
+        Heebo-Regular
+      </Text>
+      <Text style={{fontFamily: 'Heebo-Thin', fontSize: 30}}>Heebo-Thin</Text>
+      <Text style={{fontSize: 30}}>Default Font</Text>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
